fe0bea4c5edadecd06f420b6b5a7d6772a6ffaa9
djfranc <djamil.francis@gmail.com>
2018-02-16 22:27:10 -0500

    Adding project

---
+++ Dif1_Idea1of6_FCTCWLF/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dif1_Idea1of6_FCTCWLF
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
